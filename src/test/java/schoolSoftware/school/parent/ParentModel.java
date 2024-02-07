d7184a9f908c76d368fd0a5d36e3adab8075ed8d
Edwin Silvestre <a02305847@aggies.usu.edu>
2024-02-07 11:39:38 -0700

    Add Parent

---
+++ src/test/java/schoolSoftware/school/parent/ParentModel.java
@@ -0,0 +1,57 @@
+package schoolSoftware.school.parent;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "parent")
+public class ParentModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String fname;
+    private String lname;
+    private String phone;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
---
+++ src/test/java/schoolSoftware/school/parent/ParentRepository.java
@@ -0,0 +1,7 @@
+package schoolSoftware.school.parent;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParentRepository extends JpaRepository<ParentModel ,Integer> {
+}
