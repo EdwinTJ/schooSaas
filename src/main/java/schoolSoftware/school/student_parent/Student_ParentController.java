8174582311b670773da7d6f307f07846f2d7fee8
Edwin Silvestre <a02305847@aggies.usu.edu>
2024-02-07 19:26:02 -0700

    ADD student_parent relations

--- src/main/java/schoolSoftware/school/student_parent/Student_ParentController.java
+++ src/main/java/schoolSoftware/school/student_parent/Student_ParentController.java
@@ -1,9 +1,10 @@
 package schoolSoftware.school.student_parent;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import schoolSoftware.school.student_parent.exception.Student_ParentNotFoundException;
+
+import java.util.List;
 
 @RestController
 public class Student_ParentController {
@@ -15,5 +16,27 @@ public class Student_ParentController {
     Student_ParentModel newStudent_Parent(@RequestBody Student_ParentModel newStudent_Parent) {
         return repository.save(newStudent_Parent);
     }
+
+    @GetMapping("/student_parent")
+    List<Student_ParentModel> all() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/student_parent/{id}")
+    Student_ParentModel find(@PathVariable Integer id){
+        return this.repository.findById(id)
+                .orElseThrow(()->
+                        new Student_ParentNotFoundException(id));
+    }
+
+    @DeleteMapping("/student_parent/{id}")
+    String delete(@PathVariable Integer id){
+        if(!repository.existsById(id)){
+            throw new Student_ParentNotFoundException(id);
+        }
+        repository.deleteById(id);
+        return "Student_Parent with id " + id + " has been deleted.";
+    }
+
 
 }
--- src/main/java/schoolSoftware/school/student_parent/Student_ParentModel.java
+++ src/main/java/schoolSoftware/school/student_parent/Student_ParentModel.java
@@ -1,34 +1,27 @@
 package schoolSoftware.school.student_parent;
 
 import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
 import schoolSoftware.school.parent.ParentModel;
 import schoolSoftware.school.parent.ParentRepository;
 import schoolSoftware.school.student.StudentModel;
 import schoolSoftware.school.student.StudentRepository;
 
 @Entity
+@Transactional
 @Table(name = "student_parent")
-@SecondaryTables({
-        @SecondaryTable(name="parent", pkJoinColumns={
-                @PrimaryKeyJoinColumn(name="parent_id", referencedColumnName="id")
-        }),
-        @SecondaryTable(name="student", pkJoinColumns={
-                @PrimaryKeyJoinColumn(name="student_id", referencedColumnName="id")
-        })
-})
 public class Student_ParentModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
 
     @OneToOne
-//    @JoinColumn(table = "student")
-    @PrimaryKeyJoinColumn(name = "student_id", referencedColumnName = "id")
+    @JoinColumn(name = "student_id")
     private StudentModel student;
 
     @OneToOne
-//    @JoinColumn(table = "parent")
-    @PrimaryKeyJoinColumn(name = "parent_id", referencedColumnName = "id")
+    @JoinColumn(name = "parent_id")
     private ParentModel parent;
 
     public Integer getId() {
